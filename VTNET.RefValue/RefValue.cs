831137df4a2b5b9baa66bcee1492e998f05aba12
VoThanhThuan <thuan.vo@softz.vn>
2023-08-01 11:03:46 +0700

    Add project files.

---
+++ VTNET.RefValue/RefValue.cs
@@ -0,0 +1,10 @@
+﻿namespace VTNET.RefValue;
+public class RefValue<T>
+{
+    public RefValue(T? value)
+    {
+        _value = value;
+    }
+    readonly T? _value;
+    public T? Value => _value;
+}
